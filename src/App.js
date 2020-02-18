4ab835d132f8327f577b04f04446e078fcad059d
saif43 <saif.ahmed.anik.0@gmail.com>
2020-02-18 12:57:21 +0600

    npm start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
 import React, { Component } from "react";
-// eslint-disable-next-line
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
 import Login from "./Login";
-import show from "./assets/show.jpg";
-import Image from "react-bootstrap/Image";
 
 export default class App extends Component {
   render() {
-    return (
-      <Row>
-        <Col xs={6}>
-          <Image src={show} fluid />
-        </Col>
-        <Col xs={6}>
-          <Container>
-            <h1>A Trusted Better Account To Manage ID Card</h1>
-            <Login />
-          </Container>
-        </Col>
-      </Row>
-    );
+    return <Login />;
   }
 }
--- src/Login.js
+++ src/Login.js
@@ -1,27 +1,41 @@
 import React from "react";
-import { Form, Button } from "react-bootstrap";
+// eslint-disable-next-line
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import show from "./assets/show.jpg";
+import Image from "react-bootstrap/Image";
 
 export default function Login() {
   return (
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-      <br />
-      or
-      <br />
-      <Button variant="outline-primary">Register</Button>
-    </Form>
+    <Row>
+      <Col xs={6}>
+        <Image src={show} fluid />
+      </Col>
+      <Col xs={6}>
+        <Container>
+          <h1>A Trusted Better Account To Manage ID Card</h1>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+            <br />
+            or
+            <br />
+            <Button variant="outline-primary">Register</Button>
+          </Form>
+        </Container>
+      </Col>
+    </Row>
   );
 }
