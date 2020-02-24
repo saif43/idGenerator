865cda3e1c91556f02b553eb41e59806b0359313
saif43 <saif.ahmed.anik.0@gmail.com>
2020-02-24 11:19:51 +0600

    working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import Login from "./components/Login";
+import Mainpage from "./components/Main/Mainpage";
 
 export default class App extends Component {
   render() {
-    return <Login />;
+    return <Mainpage />;
   }
 }
